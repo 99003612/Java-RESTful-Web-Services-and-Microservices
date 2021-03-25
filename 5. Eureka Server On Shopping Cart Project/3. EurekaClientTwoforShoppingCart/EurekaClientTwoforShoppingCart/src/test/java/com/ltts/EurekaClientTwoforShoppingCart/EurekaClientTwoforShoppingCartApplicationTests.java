15c35624c24d974f8150e0e3babebcbb8473fd4f
stepin104338 <abhishek.vithal@ltts.com>
2021-03-25 12:35:06 +0530

    Eureka Client and Server for Shopping Cart

---
+++ 5. Eureka Server On Shopping Cart Project/3. EurekaClientTwoforShoppingCart/EurekaClientTwoforShoppingCart/src/test/java/com/ltts/EurekaClientTwoforShoppingCart/EurekaClientTwoforShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.EurekaClientTwoforShoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientTwoforShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
