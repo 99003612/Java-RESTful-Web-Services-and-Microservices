963da13edac5a8c30ed276d0059d368c92d1cc31
stepin104338 <abhishek.vithal@ltts.com>
2021-03-25 17:52:55 +0530

    Eureka Server using Database on Project

--- 6. Eureka Server using Database on Project/cart-microservice/src/test/java/com/ltts/shoppingcart/CartMicroserviceApplicationTests.java
+++ 6. Eureka Server using Database on Project/cart-microservice/src/test/java/com/ltts/shoppingcart/CartMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yugabyte.app.yugastore.cronoscheckoutapi;
+package com.ltts.shoppingcart;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CronosCheckoutApiApplicationTests {
+public class CartMicroserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
--- 6. Eureka Server using Database on Project/checkout-microservice/src/test/java/com/ltts/shoppingcart/checkout/CronosCheckoutApiApplicationTests.java
+++ 6. Eureka Server using Database on Project/checkout-microservice/src/test/java/com/ltts/shoppingcart/checkout/CronosCheckoutApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yugabyte.app.yugastore;
+package com.ltts.shoppingcart.checkout;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CartMicroserviceApplicationTests {
+public class CronosCheckoutApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
